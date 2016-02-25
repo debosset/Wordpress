ef628686c0b11917f2684e50ae29273eca143c69
debosset <debosset@gmail.com>
2016-02-25 16:08:04 +0100

    Correct image size thumbail to load

--- inc/loop-single.php
+++ inc/loop-single.php
@@ -30,7 +30,7 @@
 					<article>
 						<div class="archive-thumbail">
 							<?php 
-							the_post_thumbnail('archive-miniature', 
+							the_post_thumbnail('slider-homee',
 							array( 'class' => 'img-responsive')); 
 							?>
 						</div>
