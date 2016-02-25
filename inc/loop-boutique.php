8bfd20597362477251e727d7ec9340badb6b68b0
debosset <debosset@gmail.com>
2016-02-25 16:10:46 +0100

    Correction image size

--- inc/loop-boutique.php
+++ inc/loop-boutique.php
@@ -1,39 +1,39 @@
 <div class="listes-archives">
-	  <div class="container">
-			<div class="row">
-				<!-- Start Loop -->
-				
-				<?php
-				// Get all users order by amount of posts
-				$allUsers = get_users('orderby=post_count&order=DESC');
-				
-				
-				$users = array();
-				
-				// Remove subscribers
-				foreach($allUsers as $currentUser)
+	<div class="container">
+		<div class="row">
+			<!-- Start Loop -->
+
+			<?php
+			// Get all users order by amount of posts
+			$allUsers = get_users('orderby=post_count&order=DESC');
+
+
+			$users = array();
+
+			// Remove subscribers
+			foreach($allUsers as $currentUser)
+			{
+				if(!in_array( 'administrator', $currentUser->roles ) )
 				{
-					if(!in_array( 'administrator', $currentUser->roles ) )
-					{
-						$users[] = $currentUser;
-					}
+					$users[] = $currentUser;
 				}
-				
-				foreach($users as $user)
-				{
+			}
+
+			foreach($users as $user)
+			{
 				?>
-		
-			
-		
-			
-				
-				
+
+
+
+
+
+
 				<div class="col-sm-6 col-md-4">
 					<article>
 						<div class="archive-thumbail">
-							
+
 							<?php
-							
+
 							$finaluser = 'user_'.$user->ID;
 							$thumbboutique = get_field('thumbail', $finaluser );
 							$image = wp_get_attachment_image_src($thumbboutique, 'archive-miniature');
@@ -49,16 +49,16 @@
 						</div>
 					</article>
 				</div>
-				
-				
-				
-				
-				
+
+
+
+
+
 				<?php
-				}
-				?>
-				<!-- End Loop -->
-		
-			</div>
+			}
+			?>
+			<!-- End Loop -->
+
 		</div>
+	</div>
 </div>
